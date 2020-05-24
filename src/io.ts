6f0f710bf3b9ae9f2f1d5759a1ba3cd8852719f8
kingdaro <kingdaro@gmail.com>
2020-05-24 00:41:40 -0500

    io: create shared objects

--- src/io.ts
+++ src/io.ts
@@ -1,10 +1,14 @@
 import { BufReader } from "https://deno.land/std@0.51.0/io/bufio.ts"
 
+const encoder = new TextEncoder()
+const decoder = new TextDecoder()
+const bufReader = new BufReader(Deno.stdin)
+
 export function write(text: string) {
-  Deno.stdout.write(new TextEncoder().encode(text))
+  Deno.stdout.write(encoder.encode(text))
 }
 
 export async function readLine() {
-  const line = await new BufReader(Deno.stdin).readLine()
-  return line?.line ? new TextDecoder().decode(line.line) : ""
+  const result = await bufReader.readLine()
+  return result?.line ? decoder.decode(result.line) : ""
 }
